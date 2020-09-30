8c7d3ca99fac9f1ae2e72b41e56d175b821c73de
Josyane T <josyanestartarini@gmail.com>
2020-09-30 17:33:20 -0300

    Edição da estrutura das páginas
    Estrutura das páginas editadas

--- frontend/web/src/components/App.tsx
+++ frontend/web/src/components/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 
-
 function App() {
   return (
+      
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-      
-   
+
   );
 }
 
